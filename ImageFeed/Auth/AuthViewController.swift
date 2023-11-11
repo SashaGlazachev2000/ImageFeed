d01cfce1f12b06cfa836f6207dbe1dedf9d715d1
SashaGlazachev2000 <glazachev.sasha2000@gmail.com>
2023-11-11 08:34:04 +0300

    feat: new AuthViewController

---
+++ ImageFeed/Auth/AuthViewController.swift
@@ -0,0 +1,12 @@
+import Foundation
+import UIKit
+
+
+class AuthViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+}
---
+++ ImageFeed/Constants.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+let accessKey = "u8vkUGnrdtEPY3QH6jevpg9usGLAekap3R-n3qfTFo8"
+let secretKey = "14BqVFeX9eFPUZSgBSOGwAGkz99e5TdFzGEzj0blMkI"
+let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+let accessScope = "public+read_user+write_likes"
+let defaultBaseURL: URL = URL(string: "https://api.unsplash.com")!
