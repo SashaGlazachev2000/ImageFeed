b4fa84772359c34c9e4b3ef5b71d86eeb39d589d
SashaGlazachev2000 <glazachev.sasha2000@gmail.com>
2023-11-11 09:53:05 +0300

    feat: WebViewViewController added url

--- ImageFeed/Auth/WebViewViewController.swift
+++ ImageFeed/Auth/WebViewViewController.swift
@@ -2,13 +2,25 @@ import WebKit
 import UIKit
 
 class WebViewViewController: UIViewController {
+    
+    fileprivate let UnsplashAuthorizeURLString = "https://unsplash.com/oauth/authorize"
    
 
     @IBOutlet private weak var webView: WKWebView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        var urlComponents = URLComponents(string: UnsplashAuthorizeURLString)!
+        urlComponents.queryItems = [
+           URLQueryItem(name: "client_id", value: AccessKey),
+           URLQueryItem(name: "redirect_uri", value: RedirectURI),
+           URLQueryItem(name: "response_type", value: "code"),
+           URLQueryItem(name: "scope", value: AccessScope)
+        ]
+         let url = urlComponents.url!
         
+        let request = URLRequest(url: url)
+        webView.load(request)
     }
     
     @IBAction private func didTapBackButton(_ sender: Any) {
--- ImageFeed/Constants.swift
+++ ImageFeed/Constants.swift
@@ -1,7 +1,7 @@
 import Foundation
 
-let accessKey = "u8vkUGnrdtEPY3QH6jevpg9usGLAekap3R-n3qfTFo8"
-let secretKey = "14BqVFeX9eFPUZSgBSOGwAGkz99e5TdFzGEzj0blMkI"
-let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
-let accessScope = "public+read_user+write_likes"
-let defaultBaseURL: URL = URL(string: "https://api.unsplash.com")!
+let AccessKey = "u8vkUGnrdtEPY3QH6jevpg9usGLAekap3R-n3qfTFo8"
+let SecretKey = "14BqVFeX9eFPUZSgBSOGwAGkz99e5TdFzGEzj0blMkI"
+let RedirectURI = "urn:ietf:wg:oauth:2.0:oob"
+let AccessScope = "public+read_user+write_likes"
+let DefaultBaseURL: URL = URL(string: "https://api.unsplash.com")!
