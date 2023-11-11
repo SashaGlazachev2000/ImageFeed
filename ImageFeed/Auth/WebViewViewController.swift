a59be3ce4c2394ad709b21473f15b265d6ea2288
SashaGlazachev2000 <glazachev.sasha2000@gmail.com>
2023-11-11 09:13:36 +0300

    feat: added didTapBackButton to WebViewViewController

--- ImageFeed/Auth/WebViewViewController.swift
+++ ImageFeed/Auth/WebViewViewController.swift
@@ -1,9 +1,16 @@
-import Foundation
+import WebKit
 import UIKit
 
 class WebViewViewController: UIViewController {
+   
+
+    @IBOutlet private weak var webView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+    }
+    
+    @IBAction private func didTapBackButton(_ sender: Any) {
     }
 }
