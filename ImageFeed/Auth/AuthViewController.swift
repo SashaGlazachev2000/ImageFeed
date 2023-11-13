f354e2c34c9f562ee4c8c25c41497ca1f34d9208
SashaGlazachev2000 <glazachev.sasha2000@gmail.com>
2023-11-13 07:30:53 +0300

    feat: received a WebView token

--- ImageFeed/Auth/AuthViewController.swift
+++ ImageFeed/Auth/AuthViewController.swift
@@ -2,15 +2,26 @@ import Foundation
 import UIKit
 
 
-class AuthViewController: UIViewController {
-    
+class AuthViewController: UIViewController, WebViewViewControllerDelegate {
     @IBOutlet private weak var authButton: UIButton!
     
     private let ShowWebViewSegueIdentifier = "ShowWebView"
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == ShowWebViewSegueIdentifier {
+            guard
+                let viewController = segue.destination as? WebViewViewController
+            else { fatalError("Failed to prepare for \(ShowWebViewSegueIdentifier)") }
+            viewController.delegate = self
+        } else {
+            super.prepare(for: segue, sender: sender)
+        }
+    }
+    
+    func webViewViewController(_ vc: WebViewViewController, didAuthenticateWithCode code: String) {
+    }
+    
+    func webViewViewControllerDidCancel(_ vc: WebViewViewController) {
+        dismiss(animated: true)
     }
 }
